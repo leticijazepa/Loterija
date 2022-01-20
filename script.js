e5519b659558bcde8d2f0008883a9174ec9016aa
leticijazepa <zepaleticija@gmail.com>
2022-01-20 15:04:01 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1);
+const vardi=['Letīcija Zepa','Sabīne Merca','Signe Līce','Linda Atvare','Jurita Stikāne']
+const balvas=['Iphone 13','Macbook','Māja','Dzīvoklis','Mašīna']
+const naudaKopa=1000000;//kopeja summa
+let uzvaretajuSkaits=5;
+let rand= Math.random()*5;
+rand= Math.floor(rand);//noapalo uz leju
+console.log(vardi[rand]);//izvada konsole
