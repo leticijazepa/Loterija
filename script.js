bc024beddd756d413e0777ea0821950da258aa82
leticijazepa <zepaleticija@gmail.com>
2022-01-27 15:12:42 +0200

    cikls

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const vardi=['Letīcija Zepa','Sabīne Merca','Signe Līce','Linda Atvare','Juri
 const balvas=['Iphone 13','Macbook','Māja','Dzīvoklis','Mašīna']
 const naudaKopa=1000000;//kopeja summa
 let uzvaretajuSkaits=5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+for (let i=0; i < uzvaretajuSkaits; i++) {
 let rand= Math.random()*5;
 rand= Math.floor(rand);//noapalo uz leju
-console.log(vardi[rand]);//izvada konsole
+let uzvaretajs = vardi[rand];//izvada konsolē
+rindas.innerHTML += `
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
